681fad92e26fb9609867e44c131e6d945654dc02
kelwin014 <zxkomisxz@gmail.com>
2017-01-12 23:16:40 +0100

    after chapter 3, nice working

---
+++ main.py
@@ -0,0 +1,95 @@
+# KidsCanCode - Game Development with Pygame video series
+# Tile-based game - Part 1
+# Project setup
+# Video link: https://youtu.be/3UxnelT9aCo
+import pygame
+import sys
+import settings
+from settings import WIDTH, HEIGHT, TITLE, TILESIZE, FPS
+from sprites import Player, Wall
+from os import path
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode([WIDTH, HEIGHT])
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+        # pygame.key.set_repeat(when, inteval)
+        pygame.key.set_repeat(500, 100)
+        self.load_data()
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
+
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pygame.sprite.Group()
+        self.walls = pygame.sprite.Group()
+        
+        # Spawning walls
+        # enumerate makes row as index, example:
+        # l = ['a', 'b', 'c', 'd']
+        # for index, item in enumerate(l):
+        #   print(index, item)
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                elif tile == 'P':
+                    # Spawn player
+                    self.player = Player(self, col, row)
+                    
+
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pygame.draw.line(self.screen, settings.LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pygame.draw.line(self.screen, settings.LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(settings.BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
---
+++ sprites.py
@@ -0,0 +1,82 @@
+import pygame
+import settings
+from settings import TILESIZE, PLAYER_SPEED
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface([TILESIZE, TILESIZE])
+        self.image.fill(settings.YELLOW)
+        self.rect = self.image.get_rect()
+        self.vx, self.vy = 0, 0
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.vx = -PLAYER_SPEED
+        if keys[pygame.K_RIGHT]:
+            self.vx = PLAYER_SPEED
+        if keys[pygame.K_UP]:
+            self.vy = -PLAYER_SPEED
+        if keys[pygame.K_DOWN]:
+            self.vy = PLAYER_SPEED
+        # if running diagonal to avoid speed-up
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071
+            self.vy *= 0.7071
+        
+    def collide_with_walls(self, direction):
+        if direction == 'x':
+            hits = pygame.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vx > 0:
+                    # if movin' right
+                    self.x = hits[0].rect.left - self.rect.width
+                elif self.vx < 0:
+                    self.x = hits[0].rect.right
+                # if we hit, then we stop
+                self.vx = 0
+                self.rect.x = self.x
+                    
+        if direction == 'y':
+            hits = pygame.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vy > 0:
+                    # if movin' right
+                    self.y = hits[0].rect.top - self.rect.height
+                elif self.vy < 0:
+                    self.y = hits[0].rect.bottom
+                # if we hit, then we stop
+                self.vy = 0
+                self.rect.y = self.y
+
+    def update(self):
+        self.get_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.x = self.x
+        self.collide_with_walls('x')
+        self.rect.y = self.y
+        self.collide_with_walls('y')
+        
+        # Check if colliding with anythin')
+        
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        # Member of all_sprites groups and walls groups
+        self.groups = game.all_sprites, game.walls
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface([TILESIZE, TILESIZE])
+        self.image.fill(settings.GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
